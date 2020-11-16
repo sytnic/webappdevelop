4d4b87d4598ea0440e2823baac70b19a1974062d
sytnic <sytnic@list.ru>
2020-11-16 18:26:47 +0300

    3.1 is ready, 4.1 is almost ready: i loosed internet!

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+// Этото код вызывает алерт
+function simpleMessage(){
+	alert('На этой странице будут выполняться некоторые JavaScript элементы');
+}
+
+setTimeout(simpleMessage, 3000);
+
+// Этот код захватывает картинку по id, и перебирает массив, подставляя значения в src
+let myImage = document.getElementById("mainImage");
+
+let imageArray = ["images/golden_gate.jpg","images/old_copcar.jpg","images/red_rock_01.jpg",
+                 "images/tahoe_01.jpg","images/ventura_01.jpg","images/beach_houses.jpg"];
+let imageIndex = 0;
+
+function changeImage() {
+	myImage.setAttribute("src",imageArray[imageIndex]);
+	imageIndex++;
+	if (imageIndex >= imageArray.length) {
+		imageIndex = 0;
+	}
+}
+
+// этот код останавливает перебор картинок по клику на id тега img 
+let intervalHandle = setInterval(changeImage,5000);
+
+myImage.onclick =  function() {
+	clearInterval(intervalHandle);
+};
+
